cecfe30fe844e35b84abc89ba4edf3e1e36a17a6
Martin Sidorov <martin.sidorov27@gmail.com>
2025-01-26 11:44:14 +0200

    Fix migration

--- migrations/20250125133900_create_users_table.sql
+++ migrations/20250125133900_create_users_table.sql
@@ -7,9 +7,9 @@ CREATE TABLE users (
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
-INSERT INTO users (id, username, email)
+INSERT INTO users (id, username, email, created_at, updated_at)
 VALUES (
-        '1',
+        '60dcee7a-fb13-4971-9d97-9b829bec0231',
         'admin',
         'test@test.ee',
         '2025-01-25 13:39:00',
